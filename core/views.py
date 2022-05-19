a51884625d6d1906aee3e7e92a89dff855166f25
Marlon Almino <alminomarlon@gmail.com>
2022-05-19 09:29:20 -0300

    entendendo o django shell e shell do python

--- core/views.py
+++ core/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
 def index(request):
+  print(dir(request.user))
+  print(f"User: {request.user}")
+
+  if str(request.user) == 'AnonymousUser':
+    teste = 'Usuário não logado'
+  else:
+    teste = 'Usuário logado'
+
   context = {
     'curso': 'Programação Web com Django Framework',
-    'outro': 'Django é Show!!'
+    'outro': 'Django é Show!!',
+    'logado': teste
   }
 
   return render(request, 'index.html', context)
